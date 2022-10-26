0a233dec2000269ddf1f41d2ede8f64e27961174
DucQuan <84904517+QuanBKIT1@users.noreply.github.com>
2022-10-26 12:05:10 +0700

    addition information

--- test_code/test.py
+++ test_code/test.py
@@ -1,6 +1,7 @@
 import func
 import config
 
-print("Dataset path: ",config.fileData)
+print("Dataset path: ", config.fileData)
+print("Parameter: mL = ", config.mL, ", mU = ", config.mU, ", m = ", config.m, ", alpha = ", config.alpha)
 func.run_FCM()
 func.run_MC_FCM()
